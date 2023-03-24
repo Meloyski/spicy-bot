fa946aa9b71a532ef249a82c8d05067ab6350572
Meloyski <ryan@meloyski.com>
2023-03-24 19:38:47 -0400

    Removed editReply code and reverted back for commands in separate files

--- index.js
+++ index.js
@@ -58,27 +58,27 @@ client.on(Events.InteractionCreate, async (interaction) => {
     return;
   }
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-    await interaction.editReply("Pong again!");
-  }
-
-  // try {
-  //   await command.execute(interaction);
-  // } catch (error) {
-  //   console.error(error);
-  //   if (interaction.replied || interaction.deferred) {
-  //     await interaction.followUp({
-  //       content: "There was an error while executing this command!",
-  //       ephemeral: true,
-  //     });
-  //   } else {
-  //     await interaction.reply({
-  //       content: "There was an error while executing this command!",
-  //       ephemeral: true,
-  //     });
-  //   }
+  // if (interaction.commandName === "ping") {
+  //   await interaction.reply("Pong!");
+  //   await interaction.editReply("Pong again!");
   // }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 client.login(process.env.TOKEN);
