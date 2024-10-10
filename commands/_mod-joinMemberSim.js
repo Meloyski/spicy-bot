7a6532daf75fa0b1a90f352ce08f766623f42920
Meloyski <ryan@meloyski.com>
2024-10-10 16:05:47 -0400

    Updated available commands, archived olders ones. Added Join member experience with an auto reply and an embed

---
+++ commands/_mod-joinMemberSim.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Events } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("simulatejoin")
+    .setDescription(
+      "Simulates a new member joining the server for testing purposes."
+    )
+    .setDefaultMemberPermissions(0),
+  async execute(interaction) {
+    const member = interaction.member; // Use the command invoker as the simulated member
+
+    // Trigger the guildMemberAdd event
+    interaction.client.emit(Events.GuildMemberAdd, member);
+
+    await interaction.reply({
+      content: "Simulated a new member joining!",
+      ephemeral: true,
+    });
+  },
+};
